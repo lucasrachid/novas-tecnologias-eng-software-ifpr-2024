73bdc107a78c8c644c394d672bc39db9bb6158a7
Lucas Rachid Martins <lucasrachid@hotmail.com>
2024-07-08 20:08:03 -0300

    autenticacao JWT nas views

--- modules/views.py
+++ modules/views.py
@@ -1,21 +1,28 @@
 from django.http import JsonResponse
-from django.views import View
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-class ModulesView(View):
 
+class ModulesView(APIView):
+
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    
     def get(self, request):
         itens = [{'id': 1, 'nome': 'Item 1'}, {'id': 2, 'nome': 'Item 2'}]
-        return JsonResponse({'itens': itens})
+        return Response(itens)
 
     def post(self, request):
         novo_item = {'id': 3, 'nome': 'Novo Item'}
-        return JsonResponse({'mensagem': 'Item criado com sucesso!', 'item': novo_item}, status=201)
+        return Response({'mensagem': 'Item criado com sucesso!', 'item': novo_item}, status=201)
 
     def put(self, request):
         id_item = request.POST.get('id')
         item_atualizado = {'id': id_item, 'nome': 'Item Atualizado'}
-        return JsonResponse({'mensagem': 'Item atualizado com sucesso!', 'item': item_atualizado})
+        return Response({'mensagem': 'Item atualizado com sucesso!', 'item': item_atualizado})
 
     def delete(self, request):
         id_item = request.POST.get('id')
-        return JsonResponse({'mensagem': 'Item removido com sucesso!', 'id': id_item}, status=204)
+        return Response({'mensagem': 'Item removido com sucesso!', 'id': id_item}, status=204)
